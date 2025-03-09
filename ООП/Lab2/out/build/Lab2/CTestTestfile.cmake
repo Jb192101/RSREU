60784294effe51614a3df4a6382938170bd5c1b5
Григорий Барышев <155821386+Jb192101@users.noreply.github.com>
2025-03-09 15:04:03 +0300

    Delete ООП/Lab2/out/build/Lab2 directory

--- ООП/Lab2/out/build/Lab2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/gogat/source/repos/projects/Lab2
-# Build directory: C:/Users/gogat/source/repos/projects/Lab2/out/build/Lab2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
