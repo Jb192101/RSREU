a5f05c1051c5ef3c7e13de985defb6d237d305aa
Jb192101 <grigorijbaryshev910@gmail.com>
2025-03-14 12:02:47 +0300

    Лабораторная работа 2 по ВА

--- Вычислительные алгоритмы/Лабораторная 2/Lab2_CA/Lab2_CA/GraphWindow.xaml.cs
+++ Вычислительные алгоритмы/Лабораторная 2/Lab2_CA/Lab2_CA/GraphWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,22 +13,80 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using OxyPlot;
+using OxyPlot.Axes;
 using OxyPlot.Series;
 
 namespace Lab2_CA
 {
     public partial class GraphWindow : Window
     {
+        private static double x0 = -2;
+        private static double xn = 2;
+        private static double epsilon = 0.0001;
+
+        public PlotModel MyModel { get; set; }
+
         public GraphWindow()
         {
             InitializeComponent();
-
-            MyModelTask1 = new PlotModel { Title = "График" };
-            MyModelTask1.Series.Add(new FunctionSeries(Math.Sin, 0, 10, 0.1, "График" ));
-
-            this.DataContext = this;
+            MyModel = CreatePlotModel();
+            DataContext = this; // Устанавливаем DataContext для привязки
         }
 
-        public PlotModel MyModelTask1 { get; set; }
+        private PlotModel CreatePlotModel()
+        {
+            var plotModel = new PlotModel { Title = "График функции" };
+
+            var series = new LineSeries
+            {
+                MarkerType = MarkerType.None
+            };
+
+            var seria_point = new ScatterSeries
+            {
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 5,
+                MarkerFill = OxyColors.Red,
+            };
+
+
+            for (double x = -10; x <= 10; x += epsilon)
+            {
+                double y = MainWindow.f(x, MainWindow.a, MainWindow.b, MainWindow.c, MainWindow.d);
+                series.Points.Add(new DataPoint(x, y));
+
+                if(Math.Abs(y) < epsilon)
+                {
+                    seria_point.Points.Add(new ScatterPoint(x, y));
+                }
+            }
+
+            plotModel.Axes.Add(new LinearAxis
+            {
+                Position = AxisPosition.Bottom,
+                Minimum = x0,
+                Maximum = xn,
+                PositionAtZeroCrossing = true,
+                AxislineStyle = LineStyle.Solid,
+                IsPanEnabled = false,
+                IsZoomEnabled = false,
+            });
+            plotModel.Axes.Add(new LinearAxis
+            {
+                Position = AxisPosition.Left,
+                Minimum = x0,
+                Maximum = xn,
+                PositionAtZeroCrossing = true,
+                AxislineStyle = LineStyle.Solid,
+                IsPanEnabled = false,
+                IsZoomEnabled = false,
+            });
+
+            plotModel.Series.Add(series);
+            plotModel.Series.Add(seria_point);
+
+            return plotModel;
+        }
+
     }
 }
--- Вычислительные алгоритмы/Лабораторная 2/Lab2_CA/Lab2_CA/MainWindow.xaml.cs
+++ Вычислительные алгоритмы/Лабораторная 2/Lab2_CA/Lab2_CA/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -17,22 +18,22 @@ namespace Lab2_CA
          * Коэффициенты для уравнения ae^(bx) - 5cx^(2d)
          * По умолчанию всё равно единице
          */
-        float a = 1.0f;
-        float b = 1.0f;
-        float c = 1.0f;
-        float d = 1.0f;
+        public static float a = 1.0f;
+        public static float b = 1.0f;
+        public static float c = 1.0f;
+        public static float d = 1.0f;
 
         // Эпсилон (постоянная)
         const float epsilon = 0.0001f;
 
         // Диапазон
-        double l1 = -1 * Math.Pow(10, 5);
+        double l1 = 0;
         double l2 = Math.Pow(10, 5);
 
         // Ф-ии
 
         // Функция
-        private double f(double x)
+        public static double f(double x, double a, double b, double c, double d)
         {
             return a*Math.Pow(x, 5*b) + c*x - 0.2*d;
         }
@@ -48,23 +49,10 @@ namespace Lab2_CA
         {
             return 5 * a * b * (5 * b - 1) * Math.Pow(x, 5 * b - 2);
         }
-
-        // Фи 1 (доделать)
-        private double phi1(double x)
+        // Фи
+        private double phi(double x)
         {
-            return x;
-        }
-
-        // Фи 2 (доделать)
-        private double phi2(double x)
-        {
-            return x;
-        }
-
-        // Фи 3 (доделать)
-        private double phi3(double x)
-        {
-            return x;
+            return 0.2*d/c - a*Math.Pow(x, 5*b)/c;
         }
 
         // Ф-ии закончились
@@ -80,19 +68,20 @@ namespace Lab2_CA
             try
             {
                 // Загрузка данных
-                this.a = Convert.ToSingle(a_tb.Text);
-                this.b = Convert.ToSingle(b_tb.Text);
-                this.c = Convert.ToSingle(c_tb.Text);
-                this.d = Convert.ToSingle(d_tb.Text);
+                a = Convert.ToSingle(a_tb.Text);
+                b = Convert.ToSingle(b_tb.Text);
+                c = Convert.ToSingle(c_tb.Text);
+                d = Convert.ToSingle(d_tb.Text);
                 //Console.WriteLine("Данные загружены успешно!");
 
                 // Использование методов
+                //dihotomiya_label.Text = dihotomy_method();
                 dihotomiya_label.Text = dihotomy_method();
                 proportional_label.Text = hords_method();
                 newton_label.Text = newton_method();
                 modif_newton_label.Text = modif_newton_method();
                 combine_label.Text = combine_method();
-                iterations_label.Text = iterations_method();
+                iterations_label.Text = iterations_method(); 
                 //Console.WriteLine("Все методы прошлись успешно!");
 
             }
@@ -110,14 +99,16 @@ namespace Lab2_CA
             graphWindow.Show();
         }
 
-        // Метод дихотомии
+        // Методы
+        // Метод дихотомии (+)
         private String dihotomy_method()
         {
+            int countOfIterations = 0;
             double z, x = 0;
-            while(Math.Abs(l2 - l1) >= epsilon)
+            while(Math.Abs(l2 - l1) >= 2*epsilon)
             {
                 z = (l1 + l2) / 2;
-                if (f(z) * f(l1) < 0)
+                if (f(z, a, b, c, d) * f(l1, a, b, c, d) < 0)
                 {
                     l2 = z;
                 } else
@@ -125,61 +116,49 @@ namespace Lab2_CA
                     l1 = z;
                 }
 
-                x = (l2 + l1) / 2;
+                x = z;
+                countOfIterations++;
             }
 
-            // Восстановление значений
-            //l1 = -1 * Math.Pow(10, 5);
-            //l2 = Math.Pow(10, 5);
+            pogresh_dihotomy_label.Text = Convert.ToString(Math.Round((l2 - l1)/Math.Pow(2, countOfIterations), 15));
 
-
-            return Convert.ToString(Math.Round(x, 10));
+            return Convert.ToString(Math.Round(x, 15));
         }
 
-        // Метод хорд
+        // Метод хорд (+)
         private String hords_method()
         {
-            double q, t, x, y;
-            q = f(l1);
-            t = f(l2);
-            x = l1;
-
-            while (true)
+            double x = 1, x_prev;
+            double X;
+            if(f(l1, a, b, c, d) * f_strih_strih(l1) > 0)
             {
-                x = l1 - ((l2 - l1) * q / (t - q));
-                y = f(x);
-                if (y * q < 0)
-                {
-                    l2 = x;
-                    t = y;
-                } else
-                {
-                    l1 = x;
-                    q = y;
-                }
-
-                // Условие выхода из цикла
-                if (f(x) <= epsilon)
-                {
-                    break;
-                }
+                X = l1;
+                x_prev = l2;
+            } else
+            {
+                X = l2;
+                x_prev = l1;
             }
 
-            // Восстановление значений
-            //l1 = -1 * Math.Pow(10, 5);
-            //l2 = Math.Pow(10, 5);
+            while(x - x_prev > epsilon)
+            {
+                x = x_prev - f(x_prev, a, b, c, d) * (X - x_prev) / (f(X, a, b, c, d) - f(x_prev, a, b, c, d));
+                x_prev = x;
+            }
 
-            return Convert.ToString(Math.Round(x, 10));
+            pogresh_hords_label.Text = Convert.ToString(Math.Round(l2 - l1, 15));
+
+            return Convert.ToString(x);
         }
 
-        // Метод Ньютона
+        // Метод Ньютона (+)
         private String newton_method()
         {
             double x;
-            if (f(l1) * f_strih_strih(l1) > 0)
+            if (f(l1, a, b, c, d) * f_strih_strih(l1) > 0)
             {
                 x = l1;
-            } else if (f(l2) * f_strih_strih(l2) > 0)
+            } else if (f(l2, a, b, c, d) * f_strih_strih(l2) > 0)
             {
                 x = l2;
             } else
@@ -190,7 +169,7 @@ namespace Lab2_CA
             double h;
             while(true)
             {
-                h = f(x) / f_strih(x);
+                h = f(x, a, b, c, d) / f_strih(x);
                 x = x - h;
 
                 if (Math.Abs(h) < epsilon)
@@ -199,50 +178,90 @@ namespace Lab2_CA
                 }
             }
 
-            return Convert.ToString(Math.Round(x, 10));
+            pogresh_Newton_label.Text = Convert.ToString(Math.Round(h, 15));
+
+            return Convert.ToString(Math.Round(x, 15));
         }
 
-        // Модифицированный метод Ньютона
+        // Модифицированный метод Ньютона (+)
         private String modif_newton_method()
         {
-            return Convert.ToString(1);
-        }
-
-        // Комбинированный метод
-        private String combine_method()
-        {
-            return Convert.ToString(1);
-        }
-
-        // Метод итераций (доделать)
-        private String iterations_method()
-        {
-            double x, x1, d;
-            x = (l1 + l2) / 2;
-
-            while (true)
+            double x = 1000;
+            double x_prev = 0;
+            if(f(l1, a, b, c, d) * f_strih_strih(l1) > 0)
             {
-                if (x > 0)
-                {
-                    x1 = phi1(x);
-                } else if (x > -1)
-                {
-                    x1 = phi2(x);
-                } else
-                {
-                    x1 = phi3(x);
-                }
-
-                d = Math.Abs(x1 - x);
-                x = x1;
-
-                if (x1 < epsilon)
-                {
-                    break;
-                }
+                x = l1;
+            } else
+            {
+                x = l2;
             }
 
-            return Convert.ToString(x);
+            double c_der = f_strih(x);
+            double inacc = 100000;
+            while (x - x_prev > epsilon)
+            {
+                x_prev = x;
+                x = Math.Round(x - f(x, a, b, c, d) / c_der, 10);
+                inacc = Math.Abs(Math.Round(f(x, a, b, c, d) / Math.Abs(f_strih(x)), 10));
+            }
+
+            pogresh_modif_newton_label.Text = Convert.ToString(Math.Round(Math.Abs(x - x_prev), 10));
+
+            return Convert.ToString(Math.Round(x, 15));
+        }
+
+        // Комбинированный метод (+)
+        private String combine_method()
+        {
+            double x, y;
+            if (f(l1, a, b, c, d) * f_strih_strih(l1) > 0)
+            {
+                x = l1;
+                y = l2;
+            } else
+            {
+                x = l2;
+                y = l1;
+            }
+
+            double inacc = 1000000.0f;
+            while (inacc > epsilon)
+            {
+                x = Math.Round(x - f(x, a, b, c, d) / f_strih(x), 10);
+                y = Math.Round(y - f(y, a, b, c, d) * (x - y) / (f(x, a, b, c, d) - f(y, a, b, c, d)), 10);
+                inacc = Math.Abs((x - y));
+            }
+
+            pogresh_combine_label.Text = Convert.ToString(Math.Round(inacc, 15));
+            return Convert.ToString(Math.Round(x, 15));
+        }
+
+        // Метод итераций
+        private String iterations_method()
+        {
+            int maxIterations = 100;
+            double last = 0;
+            double x = (l1 + l2) / 2;
+            double m = Math.Max(f(l1, a, b, c, d), f(l2, a, b, c, d));
+            m = Math.Max(m, f(x, a, b, c, d));
+            double acc = 0, x_new = 1;
+
+            for(int i = 0; i < maxIterations; i++)
+            {
+                if(Math.Abs(f(x, a, b, c, d)) < epsilon)
+                {
+                    acc = Math.Round(Math.Abs(x - last), 15);
+                    break;
+                }
+                x_new = x - f(x, a, b, c, d) / m;
+                m = Math.Max(m, f_strih(x_new));
+                last = x;
+                x = x_new;
+            }
+
+            pogresh_iterations_label.Text = Convert.ToString(acc);
+
+            return Convert.ToString(Math.Round(x, 15));
         }
 
     }
