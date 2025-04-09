84f57d34bca017deab24e1709fb84e0c0617b8f3
Jb192101 <grigorijbaryshev910@gmail.com>
2025-04-09 08:24:25 +0300

    Завершение ЛР 5 ООП

--- ООП/Lab5/Set.h
+++ ООП/Lab5/Set.h
@@ -1,25 +1,321 @@
-#pragma once
+struct Vec2 {
+    float x;
+    float y;
+    
+    bool operator==(const Vec2& other) const {
+        return x == other.x && y == other.y;
+    }
+    
+    bool operator!=(const Vec2& other) const {
+        return !(*this == other);
+    }
+};
 
-#include <vector>
+// Перегрузка оператора ввода для Vec2
+std::istream& operator>>(std::istream& is, Vec2& vec) {
+    char dummy;
+    return is >> dummy >> vec.x >> dummy >> vec.y >> dummy; // Формат: {x, y}
+}
 
-template<typename T, size_t V>
+// Перегрузка оператора вывода для Vec2
+std::ostream& operator<<(std::ostream& os, const Vec2& vec) {
+    os << "{" << vec.x << ", " << vec.y << "}";
+    return os;
+}
+
+// Шаблонный класс Set
+template <typename T, size_t MAX_SIZE>
 class Set {
-    private:
-        size_t volume;
-        std::vector<T> elems;
-    public:
-        Set() : volume(0);
-        Set(const Set& other);
+private:
+    T* elements;
+    size_t length;
 
-        size_t getLength();
-        void unite(Set _set);
-        bool isExist(T _elem);
+public:
+    // Конструктор по умолчанию
+    Set() : elements(new T[MAX_SIZE]), length(0) {}
 
-        Set operator + (const T& _elem) const;
-        Set operator - (const T& _elem) const;
-        Set operator * (const Set& _set) const;
-        bool operator == (const Set& _set) const;
-        bool operator != (const Set& _set) const;
+    // Конструктор копирования
+    Set(const Set& other) : elements(new T[MAX_SIZE]), length(other.length) {
+        for (size_t i = 0; i < length; ++i) {
+            elements[i] = other.elements[i];
+        }
+    }
 
-        void printSet() const;
+    // Деструктор
+    ~Set() {
+        delete[] elements;
+    }
+
+    // Оператор присваивания
+    Set& operator=(const Set& other) {
+        if (this != &other) {
+            delete[] elements;
+            elements = new T[MAX_SIZE];
+            length = other.length;
+            for (size_t i = 0; i < length; ++i) {
+                elements[i] = other.elements[i];
+            }
+        }
+        return *this;
+    }
+
+    // Получить текущее количество элементов
+    size_t getLength() const {
+        return length;
+    }
+
+    // Проверить существование элемента
+    bool isExist(const T& elem) const {
+        for (size_t i = 0; i < length; ++i) {
+            if (elements[i] == elem) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Объединить с другим множеством
+    Set unite(const Set& other) const {
+        Set result(*this);
+        for (size_t i = 0; i < other.length; ++i) {
+            if (!result.isExist(other.elements[i]) && result.length < MAX_SIZE) {
+                result.elements[result.length++] = other.elements[i];
+            }
+        }
+        return result;
+    }
+
+    // Добавить элемент в множество
+    Set operator+(const T& elem) const {
+        Set result(*this);
+        if (!result.isExist(elem)) {
+            if (result.length < MAX_SIZE) {
+                result.elements[result.length++] = elem;
+            } else {
+                std::cerr << "Error: Maximum set size reached (" << MAX_SIZE << ")\n";
+            }
+        }
+        return result;
+    }
+
+    // Удалить элемент из множества
+    Set operator-(const T& elem) const {
+        Set result(*this);
+        for (size_t i = 0; i < result.length; ++i) {
+            if (result.elements[i] == elem) {
+                for (size_t j = i; j < result.length - 1; ++j) {
+                    result.elements[j] = result.elements[j + 1];
+                }
+                result.length--;
+                break;
+            }
+        }
+        return result;
+    }
+
+    // Пересечение двух множеств
+    Set operator*(const Set& other) const {
+        Set result;
+        for (size_t i = 0; i < length; ++i) {
+            if (other.isExist(elements[i])) {
+                result.elements[result.length++] = elements[i];
+            }
+        }
+        return result;
+    }
+
+    // Проверка на равенство множеств
+    bool operator==(const Set& other) const {
+        if (length != other.length) return false;
+        for (size_t i = 0; i < length; ++i) {
+            if (!other.isExist(elements[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Проверка на неравенство множеств
+    bool operator!=(const Set& other) const {
+        return !(*this == other);
+    }
+
+    // Вывод множества в поток
+    friend std::ostream& operator<<(std::ostream& os, const Set& set) {
+        os << "Set (" << set.length << " elements): [";
+        for (size_t i = 0; i < set.length; ++i) {
+            os << set.elements[i];
+            if (i != set.length - 1) {
+                os << ", ";
+            }
+        }
+        os << "]";
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Set& set) {
+        T elem;
+        is >> elem;
+        if (!set.isExist(elem) && set.length < MAX_SIZE) {
+            set.elements[set.length++] = elem;
+        }
+        return is;
+    }
+};
+
+// Специализация для char* (C-строк)
+template <size_t MAX_SIZE>
+class Set<char*, MAX_SIZE> {
+private:
+    char** elements;
+    size_t length;
+
+public:
+    Set() : elements(new char*[MAX_SIZE]), length(0) {
+        for (size_t i = 0; i < MAX_SIZE; ++i) {
+            elements[i] = nullptr;
+        }
+    }
+
+    Set(const Set& other) : elements(new char*[MAX_SIZE]), length(other.length) {
+        for (size_t i = 0; i < length; ++i) {
+            elements[i] = new char[strlen(other.elements[i]) + 1];
+            strcpy(elements[i], other.elements[i]);
+        }
+        for (size_t i = length; i < MAX_SIZE; ++i) {
+            elements[i] = nullptr;
+        }
+    }
+
+    ~Set() {
+        for (size_t i = 0; i < length; ++i) {
+            delete[] elements[i];
+        }
+        delete[] elements;
+    }
+
+    Set& operator=(const Set& other) {
+        if (this != &other) {
+            for (size_t i = 0; i < length; ++i) {
+                delete[] elements[i];
+            }
+            delete[] elements;
+            
+            elements = new char*[MAX_SIZE];
+            length = other.length;
+            for (size_t i = 0; i < length; ++i) {
+                elements[i] = new char[strlen(other.elements[i]) + 1];
+                strcpy(elements[i], other.elements[i]);
+            }
+            for (size_t i = length; i < MAX_SIZE; ++i) {
+                elements[i] = nullptr;
+            }
+        }
+        return *this;
+    }
+
+    size_t getLength() const {
+        return length;
+    }
+
+    bool isExist(const char* elem) const {
+        for (size_t i = 0; i < length; ++i) {
+            if (strcmp(elements[i], elem) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Set unite(const Set& other) const {
+        Set result(*this);
+        for (size_t i = 0; i < other.length; ++i) {
+            if (!result.isExist(other.elements[i]) && result.length < MAX_SIZE) {
+                result.elements[result.length] = new char[strlen(other.elements[i]) + 1];
+                strcpy(result.elements[result.length], other.elements[i]);
+                result.length++;
+            }
+        }
+        return result;
+    }
+
+    Set operator+(const char* elem) const {
+        Set result(*this);
+        if (!result.isExist(elem)) {
+            if (result.length < MAX_SIZE) {
+                result.elements[result.length] = new char[strlen(elem) + 1];
+                strcpy(result.elements[result.length], elem);
+                result.length++;
+            } else {
+                std::cerr << "Error: Maximum set size reached (" << MAX_SIZE << ")\n";
+            }
+        }
+        return result;
+    }
+
+    Set operator-(const char* elem) const {
+        Set result(*this);
+        for (size_t i = 0; i < result.length; ++i) {
+            if (strcmp(result.elements[i], elem) == 0) {
+                delete[] result.elements[i];
+                for (size_t j = i; j < result.length - 1; ++j) {
+                    result.elements[j] = result.elements[j + 1];
+                }
+                result.length--;
+                result.elements[result.length] = nullptr;
+                break;
+            }
+        }
+        return result;
+    }
+
+    Set operator*(const Set& other) const {
+        Set result;
+        for (size_t i = 0; i < length; ++i) {
+            if (other.isExist(elements[i])) {
+                result.elements[result.length] = new char[strlen(elements[i]) + 1];
+                strcpy(result.elements[result.length], elements[i]);
+                result.length++;
+            }
+        }
+        return result;
+    }
+
+    bool operator==(const Set& other) const {
+        if (length != other.length) return false;
+        for (size_t i = 0; i < length; ++i) {
+            if (!other.isExist(elements[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool operator!=(const Set& other) const {
+        return !(*this == other);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Set& set) {
+        os << "Set (" << set.length << " elements): [";
+        for (size_t i = 0; i < set.length; ++i) {
+            os << "\"" << set.elements[i] << "\"";
+            if (i != set.length - 1) {
+                os << ", ";
+            }
+        }
+        os << "]";
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Set& set) {
+        char buffer[256];
+        is.getline(buffer, sizeof(buffer));
+        
+        if (!set.isExist(buffer) && set.length < MAX_SIZE) {
+            set.elements[set.length] = new char[strlen(buffer) + 1];
+            strcpy(set.elements[set.length], buffer);
+            set.length++;
+        }
+        return is;
+    }
 };
--- ООП/Lab5/main.cpp
+++ ООП/Lab5/main.cpp
@@ -1,3 +1,221 @@
-int main() {
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include "Set.h"
+
+template <typename T>
+void testSet() {
+    const size_t MAX_SIZE = 5;
+    Set<T, MAX_SIZE> set1, set2;
+    T element;
+    int choice;
     
+    while (true) {
+        std::cout << "\n=== Testing Set<" << typeid(T).name() << "> ===" << std::endl;
+        std::cout << "Set1: " << set1 << std::endl;
+        std::cout << "Set2: " << set2 << std::endl;
+        std::cout << "1. Add element to Set1" << std::endl;
+        std::cout << "2. Remove element from Set1" << std::endl;
+        std::cout << "3. Check if element exists in Set1" << std::endl;
+        std::cout << "4. Add element to Set2" << std::endl;
+        std::cout << "5. Remove element from Set2" << std::endl;
+        std::cout << "6. Unite Set1 and Set2" << std::endl;
+        std::cout << "7. Intersection of Set1 and Set2" << std::endl;
+        std::cout << "8. Check if Set1 == Set2" << std::endl;
+        std::cout << "9. Clear sets" << std::endl;
+        std::cout << "0. Back to main menu" << std::endl;
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+        
+        switch (choice) {
+            case 1: {
+                std::cout << "Enter element to add: ";
+                std::cin >> element;
+                set1 = set1 + element;
+                break;
+            }
+            case 2: {
+                std::cout << "Enter element to remove: ";
+                std::cin >> element;
+                set1 = set1 - element;
+                break;
+            }
+            case 3: {
+                std::cout << "Enter element to check: ";
+                std::cin >> element;
+                std::cout << "Element " << (set1.isExist(element) ? "exists" : "does not exist") << " in Set1" << std::endl;
+                break;
+            }
+            case 4: {
+                std::cout << "Enter element to add: ";
+                std::cin >> element;
+                set2 = set2 + element;
+                break;
+            }
+            case 5: {
+                std::cout << "Enter element to remove: ";
+                std::cin >> element;
+                set2 = set2 - element;
+                break;
+            }
+            case 6: {
+                Set<T, MAX_SIZE> united = set1.unite(set2);
+                std::cout << "United set: " << united << std::endl;
+                break;
+            }
+            case 7: {
+                Set<T, MAX_SIZE> intersection = set1 * set2;
+                std::cout << "Intersection: " << intersection << std::endl;
+                break;
+            }
+            case 8: {
+                std::cout << "Set1 " << (set1 == set2 ? "==" : "!=") << " Set2" << std::endl;
+                break;
+            }
+            case 9: {
+                set1 = Set<T, MAX_SIZE>();
+                set2 = Set<T, MAX_SIZE>();
+                std::cout << "Sets cleared" << std::endl;
+                break;
+            }
+            case 0: {
+                return;
+            }
+            default: {
+                std::cout << "Invalid choice!" << std::endl;
+                break;
+            }
+        }
+    }
+}
+
+void testCStringSet() {
+    const size_t MAX_SIZE = 5;
+    Set<char*, MAX_SIZE> set1, set2;
+    char buffer[256];
+    int choice;
+    
+    while (true) {
+        std::cout << "\n=== Testing Set<char*> ===" << std::endl;
+        std::cout << "Set1: " << set1 << std::endl;
+        std::cout << "Set2: " << set2 << std::endl;
+        std::cout << "1. Add string to Set1" << std::endl;
+        std::cout << "2. Remove string from Set1" << std::endl;
+        std::cout << "3. Check if string exists in Set1" << std::endl;
+        std::cout << "4. Add string to Set2" << std::endl;
+        std::cout << "5. Remove string from Set2" << std::endl;
+        std::cout << "6. Unite Set1 and Set2" << std::endl;
+        std::cout << "7. Intersection of Set1 and Set2" << std::endl;
+        std::cout << "8. Check if Set1 == Set2" << std::endl;
+        std::cout << "9. Clear sets" << std::endl;
+        std::cout << "0. Back to main menu" << std::endl;
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+        
+        // Очистка буфера ввода
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        
+        switch (choice) {
+            case 1: {
+                std::cout << "Enter string to add: ";
+                std::cin.getline(buffer, sizeof(buffer));
+                set1 = set1 + buffer;
+                break;
+            }
+            case 2: {
+                std::cout << "Enter string to remove: ";
+                std::cin.getline(buffer, sizeof(buffer));
+                set1 = set1 - buffer;
+                break;
+            }
+            case 3: {
+                std::cout << "Enter string to check: ";
+                std::cin.getline(buffer, sizeof(buffer));
+                std::cout << "String \"" << buffer << "\" " << (set1.isExist(buffer) ? "exists" : "does not exist") << " in Set1" << std::endl;
+                break;
+            }
+            case 4: {
+                std::cout << "Enter string to add: ";
+                std::cin.getline(buffer, sizeof(buffer));
+                set2 = set2 + buffer;
+                break;
+            }
+            case 5: {
+                std::cout << "Enter string to remove: ";
+                std::cin.getline(buffer, sizeof(buffer));
+                set2 = set2 - buffer;
+                break;
+            }
+            case 6: {
+                Set<char*, MAX_SIZE> united = set1.unite(set2);
+                std::cout << "United set: " << united << std::endl;
+                break;
+            }
+            case 7: {
+                Set<char*, MAX_SIZE> intersection = set1 * set2;
+                std::cout << "Intersection: " << intersection << std::endl;
+                break;
+            }
+            case 8: {
+                std::cout << "Set1 " << (set1 == set2 ? "==" : "!=") << " Set2" << std::endl;
+                break;
+            }
+            case 9: {
+                set1 = Set<char*, MAX_SIZE>();
+                set2 = Set<char*, MAX_SIZE>();
+                std::cout << "Sets cleared" << std::endl;
+                break;
+            }
+            case 0: {
+                return;
+            }
+            default: {
+                std::cout << "Invalid choice!" << std::endl;
+                break;
+            }
+        }
+    }
+}
+
+int main() {
+    int choice;
+    
+    while (true) {
+        std::cout << "\n=== Main Menu ===" << std::endl;
+        std::cout << "1. Test Set<int>" << std::endl;
+        std::cout << "2. Test Set<float>" << std::endl;
+        std::cout << "3. Test Set<char*>" << std::endl;
+        std::cout << "4. Test Set<Vec2>" << std::endl;
+        std::cout << "0. Exit" << std::endl;
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+        
+        switch (choice) {
+            case 1: {
+                testSet<int>();
+                break;
+            }
+            case 2: {
+                testSet<float>();
+                break;
+            }
+            case 3: {
+                testCStringSet();
+                break;
+            }
+            case 4: {
+                testSet<Vec2>();
+                break;
+            }
+            case 0: {
+                return 0;
+            }
+            default: {
+                std::cout << "Invalid choice!" << std::endl;
+                break;
+            }
+        }
+    }
+    
+    return 0;
 }
