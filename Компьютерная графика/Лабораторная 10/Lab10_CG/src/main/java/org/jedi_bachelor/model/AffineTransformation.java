1bfd462ccbe79e46bee27dcc555b21f53778911f
Jb192101 <grigorijbaryshev910@gmail.com>
2025-05-13 19:54:15 +0300

    Завершение ЛР 10 КГ

--- Компьютерная графика/Лабораторная 10/Lab10_CG/src/main/java/org/jedi_bachelor/model/AffineTransformation.java
+++ Компьютерная графика/Лабораторная 10/Lab10_CG/src/main/java/org/jedi_bachelor/model/AffineTransformation.java
@@ -1,11 +1,20 @@
 package org.jedi_bachelor.model;
 
 public class AffineTransformation {
-    public static Line rotateLine(Line line, double angleDegrees, double pivotX, double pivotY) {
-        return line;
+    public static Line rotateLine(Line line, double angleDegrees, double a, double b) {
+        // Шаг 1
+        double[][] resultMatrix = {{Math.cos(angleDegrees), Math.sin(angleDegrees), 0},
+                {-Math.sin(angleDegrees), Math.cos(angleDegrees), 0},
+                {-a*Math.cos(angleDegrees) + b*Math.sin(angleDegrees) + a, -a*Math.sin(angleDegrees) - b*Math.cos(angleDegrees) + b, 1}};
+
+        // Шаг 2
+        double[][] result1 = multiplicationMatrixes(new double[][] {{line.getStartX(), line.getStartY(), 1}}, resultMatrix);
+        double[][] result2 = multiplicationMatrixes(new double[][] {{line.getEndX(), line.getEndY(), 1}}, resultMatrix);
+
+        return new Line(result1[0][0], result1[0][1], result2[0][0],result2[0][1]);
     }
 
-    private double[][] multiplicationMatrixs(double[][] m1, double[][] m2) {
+    private static double[][] multiplicationMatrixes(double[][] m1, double[][] m2) {
         int l11 = m1.length;
         int l12 = m1[0].length;
         int l21 = m2.length;
