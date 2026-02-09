d5a411ded073ea2f8819aa9fbb09f0806ea60806
Jb192101 <grigorijbaryshev910@gmail.com>
2026-02-09 22:53:35 +0300

    Инициализация проекта для БД

---
+++ Базы данных/Курсовая работа/src/main/java/org/jedi_bachelor/course_paper_db/CoursePaperDbApplication.java
@@ -0,0 +1,13 @@
+package org.jedi_bachelor.course_paper_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePaperDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePaperDbApplication.class, args);
+	}
+
+}
---
+++ Базы данных/Курсовая работа/src/test/java/org/jedi_bachelor/course_paper_db/CoursePaperDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jedi_bachelor.course_paper_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePaperDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
